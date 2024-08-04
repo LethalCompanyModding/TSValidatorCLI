1cb9dd6598dfd931d670dcf954ed3207e2fffb93
Robyn <rnixon98@gmail.com>
2024-08-03 20:37:58 -0500

    rework BaseTSRunner to allow multiple reasons

--- src/ThunderstoreTestTool/TSPackage/CheckRunners/BaseTSCheckRunner.cs
+++ src/ThunderstoreTestTool/TSPackage/CheckRunners/BaseTSCheckRunner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using dev.mamallama.checkrunnerlib.CheckRunners;
 using dev.mamallama.checkrunnerlib.Checks;
 
@@ -9,20 +10,7 @@ namespace TSTestTool.TSPackage.CheckRunners;
 /// <param name="ErrorLevel"></param> <summary>
 /// The highest ErrorLevel this Runner can become
 /// </summary>
-internal abstract class BaseTSCheckRunner(CheckStatus ErrorLevel = CheckStatus.Fatal) : BaseCheckRunner
+internal abstract class BaseTSCheckRunner(CheckStatus ErrorLevel = CheckStatus.Fatal) : BaseCheckRunnerLimited(ErrorLevel)
 {
-    protected CheckStatus ErrorLevel = ErrorLevel;
-    public string? Because { get; private set; }
-    protected override void UpdateState(CheckStatus IncState)
-    {
-        if ((int)IncState > (int)ErrorLevel)
-            IncState = ErrorLevel;
-
-        base.UpdateState(IncState);
-    }
-    protected virtual void SetStateAndReason(CheckStatus IncState, string Reason)
-    {
-        Because = Reason;
-        UpdateState(IncState);
-    }
+    public List<string> Because { get; } = [];
 }
