e2ba5ea9f5914fadb8ecdd918dd6c6d8be3e1bb1
Robyn <rnixon98@gmail.com>
2024-08-03 22:55:37 -0500

    make deepsource even happier

--- src/ThunderstoreTestTool/TSPackage/CheckRunners/BasicIntegrityRunner.cs
+++ src/ThunderstoreTestTool/TSPackage/CheckRunners/BasicIntegrityRunner.cs
@@ -7,7 +7,7 @@ namespace TSTestTool.TSPackage.CheckRunners;
 internal class BasicIntegrityRunner(CheckStatus ErrorLevel = CheckStatus.Fatal) : BaseTSCheckRunner(ErrorLevel)
 {
     public override string CheckID => "Basic Package Integrity";
-    public override ICheckRunner[] MyChecks { get => checks; }
+    public override ICheckRunner[] MyChecks => checks;
     private readonly ICheckRunner[] checks = [
         new FileExistsCheck("README.md", ErrorLevel),
         new JSONValidationCheckRunner("manifest.json", ErrorLevel),
