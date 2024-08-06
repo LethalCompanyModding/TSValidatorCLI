cda24303beaa848e02f20e27ef8d794206164ef6
Robyn <rnixon98@gmail.com>
2024-08-06 17:18:42 -0500

    chore: update OptionalIntegrityRunner for CRL-2.1

--- src/ThunderstoreTestTool/TSPackage/CheckRunners/OptionalIntegrityRunner.cs
+++ src/ThunderstoreTestTool/TSPackage/CheckRunners/OptionalIntegrityRunner.cs
@@ -9,8 +9,8 @@ internal class OptionalIntegrityRunner : BaseTSCheckRunner
     public override string CheckID => "Optional Package Integrity";
     public override ICheckRunner[] MyChecks => checks;
     private readonly ICheckRunner[] checks = [
-        new FileExistsCheck("Changelog.md"),
-        new FileExistsCheck("LICENSE"),
+        new FileExistsCheck("Changelog.md"){ErrorLevel = CheckStatus.Warning},
+        new FileExistsCheck("LICENSE"){ErrorLevel = CheckStatus.Warning},
     ];
 }
 
